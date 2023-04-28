9da7e70d3c0287a6cd04e47b68fe09b3f1b947df
James Chiedozie James <110996482+chiedozie013@users.noreply.github.com>
2023-04-28 23:10:14 +0100

    update on models.py

--- buildDjango/chefsTable/myapp/models.py
+++ buildDjango/chefsTable/myapp/models.py
@@ -2,10 +2,16 @@ from django.db import models
 from django.db import models
 
 # Create your models here.
+
+class MenuCategory(models.Model):
+    menu_category_name = models.CharField(max_length=200)
+
+
 class Menu(models.Model):
     name = models.CharField(max_length = 100)
     cuisine = models.CharField(max_length = 100)
     price = models.IntegerField()
+    category_id = models.ForeignKey(MenuCategory, on_delete=models.PROTECT, default=None)
 
     def __str__(self):
         return self.name + " : " + self.cuisine
